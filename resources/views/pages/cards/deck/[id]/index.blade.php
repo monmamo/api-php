664f316faf8f03cb49963e89b41470d4583c573d
Jay Bienvenu <bienvenunet@yahoo.com>
2025-03-13 21:02:00 -0500

    Minor reformat.

--- resources/views/pages/cards/deck/[id]/index.blade.php
+++ resources/views/pages/cards/deck/[id]/index.blade.php
@@ -14,29 +14,22 @@ $deck = new \Canon\Deck($id);
 
         <ul class="nav">
             <li class="nav-item">
-                Contains <?php echo $deck->count(); ?> cards. Click on a card name to view its details.
+                Contains <?php echo $deck->count(); ?> cards. Click on a card to view its details.
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/cards/deck/{{$id}}/print">Print This Deck</a>
             </li>
         </ul>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-3">
-                    <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small" hx-boost="true" hx-target="#right-section" hx-swap="innerHTML">
-
-                        <?php
-                        foreach ($deck->distinctCards as $card_number => $card_info) {
-                            $card_info = \App\Card\make($card_number);
-                        ?>
-                            <li><a href="/cards/card/<?php echo $card_number; ?>/svg" class="card-link link-body-emphasis d-inline-flex text-decoration-none rounded"><?php echo 'count'; ?> <?php echo $card_number; ?> <?php echo $card_info->name() ?? ''; ?> </a></li>
-                        <?php } ?>
-
-                    </ul>
-                </div>
-                <div class="col-9" id="right-section"></div>
+        <div class="row">
+                @foreach(array_chunk($deck->distinctCards, 4) as $chunk)
+                <div class="d-flex flex-row">
+                    @foreach ($chunk as  $card_info) 
+                       <div class="p-2"><a href="/cards/card/{{$card_info->cardNumber()}}"><x-card :cardNumber="$card_info->cardNumber()" width="200" /></a></div>
+                        @endforeach
+                            </div>
+                    @endforeach
             </div>
-        </div>
+                
 
 </x-guest-layout>
