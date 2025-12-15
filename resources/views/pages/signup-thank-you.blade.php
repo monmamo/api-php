abb8e351460f27cdea385a229b2551c45f9dfb6c
Jay Bienvenu <bienvenunet@yahoo.com>
2025-12-14 20:16:58 -0600

    Signup thank you page.

---
+++ resources/views/pages/signup-thank-you.blade.php
@@ -0,0 +1,8 @@
+<x-guest-layout>
+    <x-slot:page-title>Thank You!</x-slot>
+
+<p class="lead mb-4">Thank you for signing up for our mailing list! You will receive an email from us shortly to confirm your subscription. Please check your inbox and follow the instructions to complete the process. (If it's not in your inbox, please check your spam or junk folder.)</p>
+    
+<p class="lead mb-4">We're excited to have you on board and look forward to keeping you updated with the latest news, events, and exclusive content from Monsters Masters & Mobsters.</p>
+
+</x-guest-layout>
