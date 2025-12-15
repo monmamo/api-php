e0a2d3d347c48f8c998f88fb33b48f2f3941be0d
Jay Bienvenu <bienvenunet@yahoo.com>
2025-12-14 20:42:41 -0600

    Another minor fix.

---
+++ resources/views/pages/signup-confirmed.blade.php
@@ -0,0 +1,7 @@
+<x-guest-layout>
+    <x-slot:page-title>You're Officially In!</x-slot>
+
+<p class="lead mb-4">We're excited to have you on board and look forward to keeping you updated with the latest news, events, and exclusive content from Monsters Masters & Mobsters. Make sure your email client is set to accept messages from "monmamo.com" so you won't miss a thing. </p>
+    
+
+</x-guest-layout>
