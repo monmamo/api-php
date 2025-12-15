e47a6759c9a1ce414af06086dbf6f79651581cd6
Jay Bienvenu <bienvenunet@yahoo.com>
2025-12-14 20:37:01 -0600

    Update signup.

---
+++ resources/views/pages/signup-confirmed.blade copy.php
@@ -0,0 +1,7 @@
+<x-guest-layout>
+    <x-slot:page-title>You're Officially In!</x-slot>
+
+<p class="lead mb-4">We're excited to have you on board and look forward to keeping you updated with the latest news, events, and exclusive content from Monsters Masters & Mobsters.</p>
+    
+
+</x-guest-layout>
