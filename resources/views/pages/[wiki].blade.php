d0c5c234f83d59630c59ef4907eb6cec681c4cf8
Jay Bienvenu <bienvenunet@yahoo.com>
2025-12-28 18:35:43 -0600

    Fix wiki.

--- resources/views/pages/[wiki].blade.php
+++ resources/views/pages/[wiki].blade.php
@@ -6,6 +6,13 @@ $wiki = urldecode($wiki);
 $configuration = config('wiki.pages')[$wiki] ?? null;
 if (is_null($configuration)) abort(404);
 
+if (Str::startsWith($configuration, 'lore/')) {
+ redirect()->to('/' . $configuration)->send();
+    exit;
+}
+
+if(!file_exists(base_path("$configuration/profile.html"))) abort(404);
+
 ?>
 <x-guest-layout>
 <?= file_get_contents(base_path("$configuration/profile.html")) ?>
